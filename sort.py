0431530454647221876efc5c5f24574e24933d38
Stephen <sw_github@nzgeek.com>
2025-08-05 13:38:05 +1200

    added a boring method for contrast :)

--- sort.py
+++ sort.py
@@ -8,7 +8,7 @@ async def sort_item(item, sleep_modifier):
     await asyncio.sleep(item/sleep_modifier)
     return item
 
-async def sort_items(list_to_sort, sleep_modifier=10000):
+async def sort_items(list_to_sort, sleep_modifier=1):
     """Sort the list of items. See 'sort_item' for details on sleep_modifier"""
     results=[]
     tasks=[]
@@ -18,5 +18,12 @@ async def sort_items(list_to_sort, sleep_modifier=10000):
         results.append(await result)
     return results
 
+def boring_sort(list_to_sort):
+    """Cleaner, more efficient, basically better in every way but one,
+    it isn't as much fun!"""
+    list_to_sort.sort()
+    return list_to_sort
+
 data=[5,1,2,1.9,7,3]
 print(asyncio.run(sort_items(data)))
+#print(boring_sort(data))
