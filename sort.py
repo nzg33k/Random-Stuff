2ca8d3e6a067942113a5d95f0308300e41ee30ce
Stephen <sw_github@nzgeek.com>
2025-08-05 14:17:50 +1200

    Added comments and explanation

--- sort.py
+++ sort.py
@@ -1,4 +1,20 @@
-"""Demonstrate a silly way of sorting numbers."""
+"""Demonstrate a silly way of sorting numbers.
+
+The best way I can describe this really, is imagine you have a list of numbers
+You give a number out to each person, 
+and tell them to count to that number (divided by the modifier).
+
+Then as they come back you collect their number as it arrives.
+
+Those that have a smaller number will come back sooner, 
+those with a bigger number will take longer.
+
+The flaw with sleep_modifier
+----------------------------
+It takes x time for you to give each person their number. So the first person has a
+head start of x. These are cumulative. So the fifth item takes item/modifier + 5x to come back.
+If the gap is smaller than 5x then they will arrive out of order!
+"""
 
 import asyncio
 
@@ -12,8 +28,10 @@ async def sort_items(list_to_sort, sleep_modifier=1):
     """Sort the list of items. See 'sort_item' for details on sleep_modifier"""
     results=[]
     tasks=[]
+    # Create a task for each list item
     for item in list_to_sort:
         tasks.append(asyncio.create_task(sort_item(item, sleep_modifier)))
+    # Collect the sort_item return values as they arrive
     for result in asyncio.as_completed(tasks):
         results.append(await result)
     return results
@@ -25,5 +43,5 @@ def boring_sort(list_to_sort):
     return list_to_sort
 
 data=[5,1,2,1.9,7,3]
-print(asyncio.run(sort_items(data)))
+print(asyncio.run(sort_items(data,10000)))
 #print(boring_sort(data))
